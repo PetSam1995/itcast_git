8aa726f4cc961b5146c522379bca308bacbff2a2
zhangshuai <827358889@qq.com>
2019-01-03 17:36:09 +0800

    第一次dev提交,同时创建Customer.java,提交到本地仓库

---
+++ src/main/java/com/itcast/Customer.java
@@ -0,0 +1,13 @@
+package com.itcast;
+
+/**
+ * @ClassName Customer
+ * @Description TODO
+ * @Author zhangshuai
+ * @Date 2019/1/3 17:31
+ * @Version 1.0
+ **/
+public class Customer {
+
+    private String userId;
+}
