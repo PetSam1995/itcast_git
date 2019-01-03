40cfbabc3dfe1660fc68028953ae58d18e140a81
zhangshuai <827358889@qq.com>
2019-01-03 17:13:25 +0800

    第一次提交

---
+++ src/main/java/com/itheima.java
@@ -0,0 +1,13 @@
+package com;
+
+/**
+ * @ClassName itheima
+ * @Description TODO
+ * @Author zhangshuai
+ * @Date 2019/1/3 17:11
+ * @Version 1.0
+ **/
+public class itheima {
+
+    private Integer id;
+}
